f730f2911f033ac0be8cb7b450d038362c75a74d
Fluri Jasmin <jasmin.fluri@fhnw.ch>
2020-10-29 11:08:34 +0100

    R File for lending club loan regression added

---
+++ lending-club-loan-regression.R
@@ -0,0 +1,23 @@
+# Providing a regression model for the interest rate - int_rate
+
+# Set the working directory to the folder with the data
+setwd("C:/Users/jasmi/Dropbox/MSc-FHNW/Modules/_DataScience/Assignment")
+
+# Load csv file into data object
+allData<- read.csv("regression_train_loan.csv",header= TRUE,sep = ",",quote="\"",dec=".",fill= TRUE,na.strings= "NA",blank.lines.skip= TRUE)
+
+
+# Feature Engineering
+
+
+# Missing Value Treatment
+
+
+
+# Splitting Test/Training Data
+
+
+# Regression
+
+
+# Analysis
